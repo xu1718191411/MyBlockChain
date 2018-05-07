96e23c5160e836e6c2e47208c85aa3f35d0fd550
syoui <>
2018-05-07 17:58:59 +0900

    resolve work proof problem

--- hash.js
+++ hash.js
@@ -9,10 +9,13 @@ blockChain.addIntoChain(generateTransaction("syoui", "jesuca", 31000));
 blockChain.addIntoChain(generateTransaction("syoui", "xiaoman", 10000));
 blockChain.addIntoChain(generateTransaction("syoui", "lily", 5000));
 blockChain.addIntoChain(generateTransaction("syoui", "gemo", 400));
+blockChain.addIntoChain(generateTransaction("syoui", "ruby", 420));
+blockChain.addIntoChain(generateTransaction("syoui", "cisco", 230));
+
 
 var len = (blockChain.blockChain.length);
 
-for(i=0;i<len;i++){
+for (i = 0; i < len; i++) {
     console.log(blockChain.blockChain[i].toString());
 }
 
@@ -40,54 +43,61 @@ function generateTransaction(sender, receiver, amount) {
 
 //生成一个区块链
 function BlockChain() {
+
     this.blockChain = [];
 
     BlockChain.prototype.addIntoChain = function(transaction) {
         preHash = null;
-        if(this.blockChain.length > 0){
+        if (this.blockChain.length > 0) {
             preHash = convertIntoHash(this.blockChain[this.blockChain.length - 1].toString());
         }
-        block = new Block(this.blockChain.length, transaction,preHash,this.workProof());
+
+        var _proof = this.workProof();
+        block = new Block(this.blockChain.length, transaction, preHash, _proof);
         this.blockChain.push(block);
     }
 
-    BlockChain.prototype.getLastBlock = function(){
-        if(this.blockChain.length > 0){
+    BlockChain.prototype.getLastBlock = function() {
+        if (this.blockChain.length > 0) {
             return this.blockChain[this.blockChain.length - 1];
-        }else{
+        } else {
             return null;
         }
     }
 
 
-    BlockChain.prototype.workProof = function(){
+    BlockChain.prototype.workProof = function() {
         var proof = 0;
-        lastProof = this.getLastBlock() == null?-1:this.getLastBlock().proof
-        lastBlock = this.getLastBlock
+        lastProof = this.getLastBlock() == null ? -1 : this.getLastBlock().proof
+        lastBlock = this.getLastBlock()
 
-        if(lastBlock == null){
+
+
+        if (lastBlock == null) {
             lastHash = null
-        }else{
+        } else {
             lastHash = convertIntoHash(lastBlock.toString())
         }
 
-        while(!this.validProof(lastProof,proof,lastHash)){
-            proof ++;
+        res = this.validProof(lastProof, proof, lastHash)
+        while (!res) {
+            proof++;
+            res = this.validProof(lastProof, proof, lastHash)
         }
 
         return proof;
     }
 
-    BlockChain.prototype.validProof = function(lastProof,proof,lastHash){
-        res = convertIntoHash(lastProof+proof+lastHash+"");
-        return res.slice(0,4) == "0000"
+    BlockChain.prototype.validProof = function(lastProof, proof, lastHash) {
+        res = convertIntoHash(lastProof + proof + lastHash + "");
+        return res.slice(0, 4) == "0001"
     }
 
 };
 
 
 //生成一个区块
-function Block(_index, _transaction,_preHash,_proof) {
+function Block(_index, _transaction, _preHash, _proof) {
     //代表了它在区域链中的哪一个位置
     this.index = 0;
     this.transaction = [];
@@ -101,9 +111,7 @@ function Block(_index, _transaction,_preHash,_proof) {
     this.timestamp = Date.parse(new Date());
     this.proof = _proof;
 
-    Block.prototype.toString = function(){
-        return JSON.stringify({index:this.index,transaction:this.transaction,preHash:this.preHash,proof:this.proof});
+    Block.prototype.toString = function() {
+        return JSON.stringify({ index: this.index, transaction: this.transaction, preHash: this.preHash, proof: this.proof });
     }
-};
-
-
+}
